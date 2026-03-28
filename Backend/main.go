bd8175c83388033dc7ce099db3469a3073c1a4f0
Hifzu04 <hifzurbtech@gmail.com>
2026-03-28 21:45:11 +0530

    fix: cors

--- Backend/main.go
+++ Backend/main.go
@@ -10,8 +10,24 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173") // React URL
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
-	
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file. Make sure it exists in the root directory.")
@@ -21,13 +37,14 @@ func main() {
 	if mongoURI == "" {
 		log.Fatal("MONGO_URL is not set in the .env file")
 	}
-	
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
 	router := gin.Default()
+	router.Use(CORSMiddleware())
 
 	// API Versioning
 	v1 := router.Group("/api/v1")
